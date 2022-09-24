e3f97f26c6553a1a1efad62736878df358de70d1
emre <20974715+etherx-dev@users.noreply.github.com>
2022-09-24 22:44:41 +0100

    Create read-all-privkeys-from-wallet-dat.py

---
+++ extract-scripts/read-all-privkeys-from-wallet-dat.py
@@ -0,0 +1,8 @@
+'''Return the correct number of keys in the wallet file, checks how many bytes to read. dirty code but works'''
+
+
+import re
+for match in re.findall(b'(\x01\x01\x04[\x1e-\x20].{34})',wf):
+    key=match.hex()
+    numtoread=(int.from_bytes(bytes.fromhex(str(key[6:8])),'big'))
+    print(key[8:][:numtoread*2])
