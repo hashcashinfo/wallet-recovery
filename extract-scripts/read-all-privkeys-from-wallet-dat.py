c9a3f6206c521fc36b5eb345b1d34ae22715c801
emre <20974715+etherx-dev@users.noreply.github.com>
2022-09-24 22:45:12 +0100

    Update read-all-privkeys-from-wallet-dat.py

--- extract-scripts/read-all-privkeys-from-wallet-dat.py
+++ extract-scripts/read-all-privkeys-from-wallet-dat.py
@@ -2,6 +2,7 @@
 
 
 import re
+wf=open('./wallet.dat','rv').read()
 for match in re.findall(b'(\x01\x01\x04[\x1e-\x20].{34})',wf):
     key=match.hex()
     numtoread=(int.from_bytes(bytes.fromhex(str(key[6:8])),'big'))
