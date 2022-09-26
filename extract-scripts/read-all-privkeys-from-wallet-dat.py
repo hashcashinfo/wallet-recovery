78effad5625fb1a366e4f2cd00e7f7d0801ed682
emre <20974715+etherx-dev@users.noreply.github.com>
2022-09-26 04:49:54 +0100

    Update read-all-privkeys-from-wallet-dat.py

--- extract-scripts/read-all-privkeys-from-wallet-dat.py
+++ extract-scripts/read-all-privkeys-from-wallet-dat.py
@@ -1,13 +1,40 @@
-'''Return the correct number of keys in the wallet file, checks how many bytes to read. dirty code but works'''
-
+# Copyright (c) 2022-* Emrre Korkmaz. (https://linkedin.com/in/in-).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import re
+from pycoin.symbols.btc import network as btcnet
+from pycoin.symbols.doge import network as dogenet
 from pycoin.symbols.ltc import network as ltcnet
-wf=open('./wallet.dat','rv').read()
-for match in re.findall(b'(\x01\x01\x04[\x1e-\x20].{34})',wf):
+wf=open('/Users/em/Downloads/ec.dat','rb').read()
+secset=set()
+btccaddrset=set()
+
+#will make this a function
+for match in re.findall(b'(\x01\x01\x04[\x10-\x20].{34})',wf):
     key=match.hex()
     numtoread=(int.from_bytes(bytes.fromhex(str(key[6:8])),'big'))
     keyexpo=bytes.fromhex(key[8:][:numtoread*2])
     secint=(int.from_bytes(keyexpo,'big'))
-    keyp=ltcnet.keys.private(is_compressed=True,secret_exponent=secint)
-    print(keyp.address())
+    secset.add(secint)
+#and this
+for seci in secset:
+    keyp=btcnet.keys.private(is_compressed=True,secret_exponent=seci)
+    btccaddrset.add(keyp.address())
+#will output progress with modular option with zero third party package dependency, cross compat with py2-3
