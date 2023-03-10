6a6a17b873d3b3f0bc28afb8149af64723534f0f
emre <20974715+etherx-dev@users.noreply.github.com>
2023-03-10 04:42:22 +0000

    Update test-scan-fuzzy-bytes.py

--- research/test-scan-fuzzy-bytes.py
+++ research/test-scan-fuzzy-bytes.py
@@ -1,10 +1,17 @@
 '''do whatever you wish with this but do not run this program it is just for documentation'''
-#wget http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz
+# wget http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz
 import bit, re, os, glob
 recurse=glob.glob('/Users/em/*dat*')
-pkeyre=re.compile(b'\x01\x04\x20([\x00-\xff]{32})') # this does Not find all the posisble keys, in fact any key within the curve but not at the maximum length will have padding with zeros and the start of the regex (\x20) will vary, also i shouldn't match 32 bytes... i have a solution but this is just the Standard way of matching keys for some reason. i'll upload my better performing regex patern later.
+# this does Not find all the posisble keys, 
+# in fact any key within the curve but not at the maximum length will have padding with zeros and the start of the regex 
+# (\x20) will vary, also i shouldn't match 32 bytes... 
+# i have a solution but this is just the Standard way of matching keys for some reason. 
+# i'll upload my better performing regex patern later.
+
+pkeyre=re.compile(b'\x01\x04\x20([\x00-\xff]{32})') 
 def returnF(filename): return(open(filename,'rb'))
-addrdb=sqlite3.connect('/Users/em/Documents/btc-addr-bal-hashcash.db') # addr,balance.tsv indexed by the address column after importing in sqlite3.
+# addr,balance.tsv indexed by the address column after importing in sqlite3.
+addrdb=sqlite3.connect('/Users/em/Documents/btc-addr-bal-hashcash.db') 
 pubkeycollection=set()
 
 for filepath in recurse:
