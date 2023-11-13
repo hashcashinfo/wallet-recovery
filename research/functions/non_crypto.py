86079e4794f9be25ace4dd4bc8df75dedc8e710c
Emre Korkmaz <20974715+etherx-dev@users.noreply.github.com>
2023-11-14 00:07:27 +0300

    seperate the expressions

--- research/functions/non_crypto.py
+++ research/functions/non_crypto.py
@@ -1,3 +1,4 @@
 # fetch and return clean lines. 
 def readLines(filePath):
     return(open(filePath,'r').read().splitlines())
+
---
+++ research/functions/regular_expressions.py
@@ -0,0 +1,2 @@
+
+xpubRE =  re.compile(r"([xyYzZtuUvV]pub[1-9A-HJ-NP-Za-km-z]{79,108})")
